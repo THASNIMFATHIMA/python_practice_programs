2d30474ef1535d21e0d1673d85bed99aad77c830
THASNIMFATHIMA <thasnimfathima20@gmail.com>
2025-02-04 14:03:11 +0530

    Add files via upload

---
+++ evenorodd.py
@@ -0,0 +1,17 @@
+a=11
+if(a%2==0):
+    {
+    print("the number is even")
+    }
+else:{
+    print ("the number is odd")
+    }
+
+a=int(input("enter the number:"))
+if(a%2==0):
+    {
+    print("the number is even")
+    }
+else:{
+    print ("the number is odd")
+    }
